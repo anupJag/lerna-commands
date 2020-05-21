68abeb0d0ae26ae1069c6cad9a90942503f61b08
anupJag <anupam.jagatdeo@gmail.com>
2020-05-21 18:06:51 +0530

    Alpha to graduate

--- packages/component/src/index.js
+++ packages/component/src/index.js
@@ -3,3 +3,5 @@ console.log("Hi I am learning lerna");
 console.log("Releasing minor with conventional commits");
 
 console.log("Will create an alpha build");
+
+console.log("Bump up alpha to graduate");
