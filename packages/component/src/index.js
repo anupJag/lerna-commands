249a06cf0a944f8278c546eceb9433266e588ad9
anupJag <anupam.jagatdeo@gmail.com>
2020-05-21 18:09:22 +0530

    Bump up version minor

--- packages/component/src/index.js
+++ packages/component/src/index.js
@@ -5,3 +5,5 @@ console.log("Releasing minor with conventional commits");
 console.log("Will create an alpha build");
 
 console.log("Bump up alpha to graduate");
+
+console.log("Pushing Minor release");
